781c2b84cb06ed90d6c889320b65371b1720b92b
cadler22 <80167381+cadler22@users.noreply.github.com>
2022-12-23 19:27:56 -0500

    Add files via upload
    New version with fixes

--- temp#1.py
+++ temp#1.py
@@ -4,114 +4,59 @@ import pandas as pd
 import csv 
 
 # opening and reading data from records.csv file 
-data = pd.read_csv(r'/Users/calistaadler/Downloads/records.csv')
+data = pd.read_csv(r'/Users/calistaadler/Downloads/DFR/records.csv')
 
-# writing into new csv file 
-f = open('/Users/calistaadler/Downloads/tempout.csv', 'w')
-writer = csv.writer(f)
-
-# creates column titles 
-header = ['Timestamp', 'ModA Temp', 'ModB Temp', 'ModC Temp', 'Gate Driver Board Temp']
-writer.writerow(header)
-
-# used to parse through each row 
-i = 0
-n = 1
+# creating new data frame 
+output = pd.DataFrame(columns=['Timestamp','ModA Temp', 'ModB Temp', 'ModC Temp', 'Gate Driver Board Temp'])
 
 # id for temperature #1
 temp1_id = '00A0'
 
-# replaces square brackets and single quotes for correct formatting and type conversions
-def replace(var):
-    var = var.replace("]","")
-    var = var.replace("[","")
-    var = var.replace("'","")
-
 # iterates through each row in original records file
 for index, row in data.iterrows():
     
-    year = data['year'][i:n].tolist()
-    year = str(year)
-    replace(year)
-    
-    month = data['month'][i:n].tolist()
-    month = str(month)
-    replace(month)
 
-    day = data['day'][i:n].tolist()
-    day = str(day)
-    replace(day)
-    
-    hour = data['hour'][i:n].tolist()
-    hour = str(hour)
-    replace(hour)
-    
-    minute = data['minutes'][i:n].tolist()
-    minute=str(minute)
-    replace(minute)
+    year = row['year']
+    month = row['month']
+    day = row['day']
+    hour = row['hour']
+    minute = row['minutes']
+    second = row['seconds']
 
-    second = data['seconds'][i:n].tolist()
-    second = str(second)
-    replace(second)
-    
-    new_row = [''.join([month, '/', day, '/', year, " ",hour, ':', minute, ':', second])]
-   
-    id = data['id'][i:n].tolist()
-    id = str(id)
-    replace(id)
+    new_row = [''.join([str(month), '/', str(day), '/', str(year), " ", str(hour), ':', str(minute), ':', str(second)])]
 
+    curr_id = row['id']
+    
     # converts each byte from hex to decimal if correct id for temperature #1
-    if temp1_id in data['id'][i:n].tolist():
-        byte0 = data['data0'][i:n].tolist()
-        byte0 = str(byte0)
-        replace(byte0)
+    if temp1_id == curr_id:
+        byte0 = row['data0']
         byte0 = int(byte0, 16)
-        byte0 = str(byte0)
 
-        byte1 = data['data1'][i:n].tolist()
-        byte1 = str(byte1)
-        replace(byte1)
+        byte1 = row['data1']
         byte1 = int(byte1, 16)
-        byte1 = str(byte1)
-
-        byte2 = data['data2'][i:n].tolist()
-        byte2 = str(byte2)
-        replace(byte2)
-        byte2 = int(byte2, 16)
-        byte2 = str(byte2)
-
-        byte3 = data['data3'][i:n].tolist()
-        byte3 = str(byte3)
-        replace(byte3)
-        byte3 = int(byte3, 16)
-        byte3 = str(byte3)
-
-        byte4 = data['data4'][i:n].tolist()
-        byte4 = str(byte4)
-        replace(byte4)
-        byte4 = int(byte4, 16)
-        byte4 = str(byte4)
         
-        byte5 = data['data5'][i:n].tolist()
-        byte5 = str(byte5)
-        replace(byte5)
+        byte2 = row['data2']
+        byte2 = int(byte2, 16)
+       
+        byte3 = row['data3']
+        byte3 = int(byte3, 16)
+
+        byte4 = row['data4']
+        byte4 = int(byte4, 16)
+        
+        byte5 = row['data5']
         byte5 = int(byte5, 16)
-        byte5 = str(byte5)
 
-        byte6 = data['data6'][i:n].tolist()
-        byte6 = str(byte6)
-        replace(byte6)
+        byte6 = row['data6']
         byte6 = int(byte6, 16)
-        byte6 = str(byte6)
 
-        byte7 = data['data7'][i:n].tolist()
-        byte7 = str(byte7)
-        replace(byte7)
+        byte7 = row['data7']
         byte7 = int(byte7, 16)
-        byte7 = str(byte7)
         
         new_row = str(new_row)
-        replace(new_row)
+        new_row = new_row.replace("'","")
+        new_row = new_row.replace("[","")
+        new_row = new_row.replace("]","")
 
         # calculates temperature for each type using 2 bytes
         modA = int(byte0) + 256*int(byte1)
@@ -119,13 +64,11 @@ for index, row in data.iterrows():
         modC = int(byte4) + 256*int(byte5)
         gate_driver = int(byte6) + 256*int(byte7)
 
-        # writes all data into new csv file
-        writer.writerow([new_row, modA, modB, modC, gate_driver])
-     
-    i = i + 1
-    n = n + 1
-    
-output = pd.read_csv(r'/Users/calistaadler/Downloads/tempout.csv')
+        # appends items to dataframe
+        output.loc[len(output.index)] = [new_row, modA/10, modB/10, modC/10, gate_driver/10]
 
-f.close()
+        
+# stores final data frame in csv file 
+output.to_csv(r'/Users/calistaadler/Downloads/DFR/tempout.csv')
+
 
